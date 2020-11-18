71d8784bf45caabd77d951af5c9d478201c3cafe
Federico <federico.scarpati1989@gmail.com>
2020-11-18 15:10:30 +0100

    Fix numeri
    Fix calcolo numeri

--- Gioco dei dadi/assets/js/main.js
+++ Gioco dei dadi/assets/js/main.js
@@ -4,9 +4,9 @@ var numberRandom1 = Math.floor(Math.random() * 5) + 1;
 var numberRandom2 = Math.floor(Math.random() * 5) + 1;
 // Sia per il giocatore sia per il computer
 var player = numberRandom1;
-alert("Hai lanciato i dadi! Il tuo numero è " + player);
+alert("Hai lanciato i dadi! Il tuo numero è il " + player);
 var pc = numberRandom2;
-alert("Il tuo avversario ha lanciato i dadi! Il suo numero è " + pc);
+alert("Il tuo avversario ha lanciato i dadi! Il suo numero è il " + pc);
 // Stabilire il vincitore, in base a chi fa il punteggio più alto
 if (player > pc) {
   alert("Hai vinto")
